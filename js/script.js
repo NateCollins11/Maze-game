0b80fe9e300d943e68fa8eb23f941a345a94f79c
Nathaniel Collins <nathanielcollins@nathaniels-mbp.lan>
2021-10-27 15:00:02 -0400

    iterating

--- js/script.js
+++ js/script.js
@@ -4,34 +4,36 @@ var path = [[0,0]]
 var play_path = [[0,0]]
 var allow_move = true
 var allow_reset = false
+var game_size = 5
 
 function setUp(){
+    var grid_size = document.getElementById("grid_size")
+    if (grid_size.value == 'rand_size'){
+        game_size = Math.floor(Math.random()*7 + 3)
+    } else {
+        game_size = parseInt(grid_size.value)
+    }
     resetBoard()
+        console.log(game_size)
         var successful_path = false
         while (successful_path == false){
             path = [[0,0]]
             var i = 0;
-            while ((path[path.length - 1][0] != 4 || path[path.length - 1][1] != 4) && (i < 60)){
+            while ((path[path.length - 1][0] != game_size - 1 || path[path.length - 1][1] != game_size - 1) && (i < 30 + (game_size * 10))){
                 i++
                 var repeat = false;
                 var neg_toggle = 1
                 var next_square_distance = [1, 0]
                 var path_direction = Math.random() < .5
                 var path_negativity = Math.random() < .4
-                console.log(path_direction)
-                console.log(path_negativity)
                 if (path_direction == true){
                     next_square_distance = [0, 1]
                 }
                 if (path_negativity == true){
-                    console.log('backwards')
                     neg_toggle = -1
                 }
-                console.log(next_square_distance)
                 var new_pos = [(path[path.length - 1][0] + (next_square_distance[0]*neg_toggle)), (path[path.length - 1][1] + (next_square_distance[1]*neg_toggle))]
-                console.log(new_pos);
-                
-                if (new_pos[0] >= 0 && new_pos[0] <= 4 && new_pos[1] >= 0 && new_pos[1] <= 4){
+                if (new_pos[0] >= 0 && new_pos[0] <= game_size - 1 && new_pos[1] >= 0 && new_pos[1] <= game_size - 1){
                     
                     for (let count = 0; count < path.length; count++){
                         if (String(path[count]) == String(new_pos)){
@@ -41,32 +43,27 @@ function setUp(){
                     if (repeat == false){
                     path.push(new_pos)
                     }else{
-                        console.log("already been there!")
                     }
-                    console.log(path[path.length - 1])
                 }else{
-                    console.log('cant go there')
                 }
-                if (path[path.length - 1][0] == 4 && path[path.length - 1][1] == 4){
-                    if (path.length > 17){
-                    console.log('FINISH')
-                    successful_path = true
-                    i = 60
+                if (path[path.length - 1][0] == game_size - 1 && path[path.length - 1][1] == game_size - 1){
+                    if (path.length > (game_size * 2 + 1)){
+                        successful_path = true
+                        i = 60
                     }
                 }
             }
-            console.log(path)
-            if (path[path.length - 1][0] == 4 && path[path.length - 1][1] == 4){
-                if (path.length > 17){
-                console.log('FinisH')
+            if (path[path.length - 1][0] == game_size - 1 && path[path.length - 1][1] == game_size - 1){
+                if (path.length > (game_size * 3 - 1)){
                 successful_path = true
                 }
             }
         
         }
         console.log(path)
+        document.getElementById("game_text_box").innerHTML = "Try to reach the end"
 }
-setUp();
+
 
 function getKeyAndMove(e){				
     var key_code=e.which||e.keyCode;
@@ -87,7 +84,7 @@ function getKeyAndMove(e){
             if (allow_move == true){
             move('down');}
             break;
-        case 82: //r
+        case 82: //r key
             if (allow_reset == true){
             resetBoard();}
             break;
@@ -111,14 +108,14 @@ function move(dir){
     }
     var next_pos = [current_pos[0] + distance[0], current_pos[1] + distance[1]]
 
-    if (next_pos[0] >= 0 && next_pos[0] <= 4 && next_pos[1] >= 0 && next_pos[1] <= 4){
+    if (next_pos[0] >= 0 && next_pos[0] <= game_size - 1 && next_pos[1] >= 0 && next_pos[1] <= game_size - 1){
        if (next_pos[0] == path[play_path.length][0] && next_pos[1] == path[play_path.length][1]){
             play_path.push(next_pos)
-            if (next_pos[0] == 4 && next_pos[1] == 4){
+            if (next_pos[0] == game_size - 1 && next_pos[1] == game_size - 1){
             drawNextSquare(next_pos, 'gold', dir)
-            alert('You Won')
+            document.getElementById("game_text_box").innerHTML = "You Won!!! Play Again?"
             }else {
-                drawNextSquare(next_pos, 'green', dir)    
+                drawNextSquare(next_pos, 'blue', dir)    
             }
        }else{
             drawNextSquare(next_pos, 'red', dir)
@@ -135,24 +132,28 @@ function move(dir){
 function drawNextSquare(pos, color, dir){
     var canvas = document.getElementById("board");
     var c = canvas.getContext("2d");
-    var h = canvas.height/5;
-    var w = canvas.width/5;
+    var h = canvas.height/game_size;
+    var w = canvas.width/game_size;
 
 
     c.fillStyle = color
     
-    c.fillRect((pos[0] * w + w)-(2*(w/3)), (pos[1] * h + h)-(2*(h/3)), w/3, h/3)
+    c.beginPath();
+    c.arc(pos[0] * w + w/2, pos[1] * h + h/2, w/9 , 0, 2*Math.PI);
+    c.fill();
+
+
     if (dir == 'right'){
-    c.fillRect(((pos[0]) * w) + (7*w/12), ((pos[1]) * h) + (5*h/12), w * -1, h/6)
+    c.fillRect(((pos[0]) * w) + (7*w/12), ((pos[1]) * h) + (11*h/24), w * -1, h/12)
     }
     if (dir == 'left'){
-        c.fillRect(((pos[0]) * w) + (5*w/12), ((pos[1]) * h) + (5*h/12), w, h/6)
+        c.fillRect(((pos[0]) * w) + (5*w/12), ((pos[1]) * h) + (11*h/24), w, h/12)
     }
     if (dir == 'up'){
-        c.fillRect(((pos[0]) * w) + (5*w/12), ((pos[1]) * h) + (5*h/12), w/6, h)
+        c.fillRect(((pos[0]) * w) + (11*w/24), ((pos[1]) * h) + (5*h/12), w/12, h)
     }
     if (dir == 'down'){
-        c.fillRect(((pos[0]) * w) + (5*w/12), ((pos[1]) * h) + (7*h/12), w/6, -1 * h)
+        c.fillRect(((pos[0]) * w) + (11*w/24), ((pos[1]) * h) + (7*h/12), w/12, -1 * h)
     }
 }
 
@@ -165,28 +166,21 @@ function prepareResetGame(){
 function resetBoard(){
     var canvas = document.getElementById("board");
     var c = canvas.getContext("2d");
-    var h = canvas.height/5;
-    var w = canvas.width/5;
-    c.fillStyle = 'antiquewhite'
-    c.fillRect(0,0,5*w, 5*h)
-    c.moveTo(w, 0);
-    c.lineTo(w, h * 5);
-    c.moveTo(w * 2, 0);
-    c.lineTo(w * 2, h * 5)
-    c.moveTo(w * 3, 0);
-    c.lineTo(w * 3, h * 5)
-    c.moveTo(w * 4, 0);
-    c.lineTo(w * 4, h * 5)
-    c.moveTo(0, h);
-    c.lineTo(w * 5, h);
-    c.moveTo(0, h * 2);
-    c.lineTo(w * 5, h * 2)
-    c.moveTo(0, h * 3);
-    c.lineTo(w * 5, h * 3)
-    c.moveTo(0, h * 4);
-    c.lineTo(w * 5, h * 4)
-    c.stroke();
-    c.fillStyle = "green"
+    var h = canvas.height/game_size;
+    var w = canvas.width/game_size;
+    c.fillStyle = 'black'
+    c.fillRect(0,0,game_size*w, game_size*h)
+    for (var line_counter = 1; line_counter < game_size; line_counter++){
+        c.beginPath();
+        c.strokeStyle = 'green'
+        c.moveTo(line_counter * w, 0);
+        c.lineTo(line_counter * w, h * game_size);
+        c.moveTo(0, line_counter * h);
+        c.lineTo(w * game_size, line_counter * h)
+        c.stroke()
+    }
+
+    c.fillStyle = "blue"
     c.fillRect(w-(2*(w/3)), h-(2*(h/3)), w/3, h/3)
     play_path = [[0,0]]
     allow_move = true
